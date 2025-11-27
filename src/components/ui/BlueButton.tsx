462d4e55e1059e179745174faaa7a47e1788df1d
CHRINGE_LAPTOP\workc <work.christian.allevi@gmail.com>
2025-11-27 16:39:12 +0100

    update mailto button email

--- src/components/ui/BlueButton.tsx
+++ src/components/ui/BlueButton.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 function BlueButton({text}: {text:string}) {
   return (
-    <Link href="mailto:marketing@easypmisolutions.it" target='blank' className='bg-pmi-blue px-3 py-2 w-fit rounded-3xl text-white'>
+    <Link href="mailto:info@easypmisolutions.it" target='blank' className='bg-pmi-blue px-5 py-3 w-fit rounded-3xl text-white'>
         {text}
     </Link>
   )
